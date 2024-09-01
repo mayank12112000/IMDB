a7dbf12fa73bf5fc7c850a38f1a7ec4a55819480
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-09-01 20:19:48 +0530

    more detials added

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import "./movieDetails.css"
 export default function MovieDetails() {
     const [movie,setMovie] = useState(null)
@@ -21,9 +21,29 @@ export default function MovieDetails() {
             <img className="movie-detail-picture" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />
             </div>
             <div className="col-sm my-2">
-                <h2>{movie.title}</h2>
+                <h2 className=''>{movie.title} </h2>
+
+                {movie.adult ? <p className='text-danger'>A rated</p> : <></>} 
                 <p>{movie.overview}</p>
             </div>
+                
+        </div>
+        <div className="genres my-3 d-flex align-items-center">
+            <div><span className='fs-5'>Genre:</span></div>
+        {movie && movie.genres.map((genre)=>(
+                        <div className="mx-2 genre">
+                        <span>{genre.name}</span>
+                    </div>
+                ))}
+        </div>
+        <div className="d-flex my-2 justify-content-between align-items-center">
+        <span className='fs-5'>Useful Links</span>
+        <Link to={"/"}>
+            <button className='btn btn-danger'>Homepage <i className="fa fa-external-link" aria-hidden="true"></i></button>
+        </Link>
+        <a target='_blank' href={movie.homepage}>
+            <button className='btn btn-warning'>Homepage <i className="fa fa-external-link" aria-hidden="true"></i></button>
+        </a>
         </div>
     </div>
   )
